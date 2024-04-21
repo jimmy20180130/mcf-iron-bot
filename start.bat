794a1c1659c1e0581fad284a71b88c28c0de2e16
XiaoXi_TW <68285469+jimmy20180130@users.noreply.github.com>
2024-04-21 23:28:13 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,8 +3,12 @@ cls
 echo bot will restart when it crashes
 title Jimmy Bot
 
+call npm install .
+
+title Jimmy Bot
+
 :StartServer
 echo (%time%) starting the bot
-start /wait node %~dp0/mcf-iron-bot-main/start.js
+start /wait node start.js
 echo (%time%) restarting the bot
 goto StartServer
